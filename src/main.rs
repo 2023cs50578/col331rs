9b0cc994cd89749e4441b33fcb9c84510d6d36ed
Naman Garg <ngarg1270@gmail.com>
2026-02-09 17:33:54 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+use core::arch::asm;
+
+fn outw(port: u16, data: u16) {
+  unsafe {
+    asm!("out dx, ax", in("dx") port, in("ax") data);
+  }
+}
+
+fn halt() -> ! {
+  outw(0x604, 0x2000);
+  // For older versions of QEMU, 
+  outw(0xB004, 0x2000);
+  loop {}
+}
+
+#[no_mangle]
+fn entryofrust() -> ! {
+  halt();
+}
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
